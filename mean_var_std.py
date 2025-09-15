0b44524bc84af19e6d5404fdefb3cfcb61d9200d
Cathulhu17 <fontanella.luciano8@gmail.com>
2025-09-15 19:37:52 -0300

    Update mean_var_std.py
    justificativo https://medium.com/@rubenmndez/desviaci%C3%B3n-est%C3%A1ndar-y-varianza-3ba43e63f155
    https://programacionpython80889555.wordpress.com/2018/06/24/calculando-medias-varianzas-y-desviaciones-con-numpy-ejercicio-basico-en-python/

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,49 @@
 import numpy as np
 
 def calculate(list):
+    if len(numbers) !=9:
+         raise ValueError("La lista debe contener nueve números")
+        
+    matrix = np.array(numbers).reshape(3,3)
 
-
+    calculated {
+        'mean': [
+            matrix.mean(axis=0).tolist(),
+            matrix.mean(axis=1).tolist(), 
+            matrix.mean().item()
+        ],
+        
+      'variance': [
+            matrix.var(axis=0).tolist(),
+            matrix.var(axis=1).tolist(), 
+            matrix.var().item()
+            ],    
+      
+  'standard deviation': [
+            matrix.std(axis=0).tolist(),
+            matrix.std(axis=1).tolist(), 
+            matrix.std().item()
+            ],
+        
+  'max': [
+            matrix.max(axis=0).tolist(),
+            matrix.max(axis=1).tolist(), 
+            matrix.max().item()
+            ],,
+        
+  'min': [
+            matrix.min(axis=0).tolist(),
+            matrix.moin(axis=1).tolist(), 
+            matrix.min().item()
+            ],
+        
+  'sum': [
+            matrix.sum(axis=0).tolist(),
+            matrix.sum(axis=1).tolist(), 
+            matrix.sum().item()
+            ]
+        
+    }
 
 
     return calculations
